68ad3d32edb67e3410cdbf4738691560b97654fd
c <c@g.hk>
2017-12-17 18:29:53 +0800

    init

---
+++ src/main/java/com/bewantbe/maidenvoyage/MaidenvoyageApplication.java
@@ -0,0 +1,12 @@
+package com.bewantbe.maidenvoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidenvoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidenvoyageApplication.class, args);
+	}
+}
