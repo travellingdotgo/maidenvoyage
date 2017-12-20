edcbd7d8ce8d46de0b0873cece40ac1ad297e060
c <c@g.hk>
2017-12-20 10:06:24 +0800

    update command para

--- src/main/java/com/bewantbe/maidenvoyage/MaidenvoyageApplication.java
+++ src/main/java/com/bewantbe/maidenvoyage/MaidenvoyageApplication.java
@@ -3,10 +3,19 @@ package com.bewantbe.maidenvoyage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+
 @SpringBootApplication
 public class MaidenvoyageApplication {
 
+	//
+	@Value("${trand}")
+	private static String trand;
+
 	public static void main(String[] args) {
+		System.out.println("执行完成: " + trand);
 		SpringApplication.run(MaidenvoyageApplication.class, args);
 	}
 }
