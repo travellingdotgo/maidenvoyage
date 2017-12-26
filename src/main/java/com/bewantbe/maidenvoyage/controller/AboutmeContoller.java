0ee79a105519ab31091db7f5995f73d0a6326885
c <c@g.hk>
2017-12-26 09:39:04 +0800

    add ModelAndView redirect

--- src/main/java/com/bewantbe/maidenvoyage/controller/AboutmeContoller.java
+++ src/main/java/com/bewantbe/maidenvoyage/controller/AboutmeContoller.java
@@ -2,20 +2,24 @@ package com.bewantbe.maidenvoyage.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 
-@RestController
+@Controller
 public class AboutmeContoller {
 
     public final static String NEWLINE = "</br>";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/aboutme", method = RequestMethod.GET)
-    public String getAboutme() {
-        return "Hello, about me";
+    public ModelAndView getAboutme() {
+        //return "Hello, about me";
+        return new ModelAndView("redirect:/cv");
     }
+
 
 }
